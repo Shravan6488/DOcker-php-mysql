4970bbb8d4bea1ae570ae4e6d3905b0324f5a3fe
Shravan6488 <32151014+Shravan6488@users.noreply.github.com>
2019-01-31 14:13:10 +0800

    Create dockerexecute.sh

---
+++ dockerexecute.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+docker build \
+  --file .docker/Dockerfile \
+  -t laravel-docker .
+  
+docker run --rm -p 8080:80 laravel-docker
+docker run \
+  --name laravel-app \
+  --rm -p 8080:80
+  laravel-docker
+  
+ docker-compose up --build
+ chmod -R o+rw bootstrap/ storage/
+ composer require predis/predis
+ docker-compose exec app /bin/bash
+ 
